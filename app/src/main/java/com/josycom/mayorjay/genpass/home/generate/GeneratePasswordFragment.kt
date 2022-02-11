27ed51d822f97992aeeab5ed33bbbd6a5ab00f45
mayorjay <joseolu4gsm@yahoo.com>
2022-02-11 20:24:37 +0100

    feat: Implement Password generation

--- app/src/main/java/com/josycom/mayorjay/genpass/home/generate/GeneratePasswordFragment.kt
+++ app/src/main/java/com/josycom/mayorjay/genpass/home/generate/GeneratePasswordFragment.kt
@@ -1,12 +1,19 @@
 package com.josycom.mayorjay.genpass.home.generate
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.josycom.mayorjay.genpass.databinding.FragmentGeneratePasswordBinding
+import com.josycom.mayorjay.genpass.util.Utilities
+import org.apache.commons.lang3.StringUtils
 
 class GeneratePasswordFragment : Fragment() {
 
@@ -23,7 +30,68 @@ class GeneratePasswordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         viewModel = ViewModelProvider(this)[GeneratePasswordViewModel::class.java]
+
+        setupSpinnerAdapter()
+        setupListeners()
+        observePassword()
+    }
+
+    private fun setupSpinnerAdapter() {
+        ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, viewModel.getPasswordTypes()).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spPasswordType.adapter = this
+        }
+    }
+
+    private fun observePassword() {
+        viewModel.password.observe(viewLifecycleOwner, { value ->
+            binding.tvPassword.text = value
+            binding.ivCopy.isVisible = StringUtils.isNotBlank(value)
+            binding.ivShare.isVisible = StringUtils.isNotBlank(value)
+        })
+    }
+
+    private fun setupListeners() {
+        binding.spPasswordType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                viewModel.passwordType.value = adapterView?.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+            }
+        }
+
+        binding.etPasswordLength.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                viewModel.passwordLength.value = s?.toString()
+            }
+        })
+
+        binding.btGenerate.setOnClickListener {
+            val message = viewModel.validateInputs()
+            if (StringUtils.isBlank(message)) {
+                viewModel.password.value = ""
+                val password = viewModel.generatePassword(viewModel.passwordType.value!!, viewModel.passwordLength.value?.toInt() ?: 0)
+                viewModel.password.value = password
+            } else {
+                Utilities.showToast(message, requireContext())
+            }
+        }
+
+        binding.ivCopy.setOnClickListener {
+            Utilities.copyContentToClipboard(viewModel.password.value!!, requireContext())
+            Utilities.showToast("Password copied", requireContext())
+        }
+
+        binding.ivShare.setOnClickListener {
+            Utilities.shareContent(viewModel.password.value!!, requireContext())
+        }
     }
 }
--- app/src/main/java/com/josycom/mayorjay/genpass/home/generate/GeneratePasswordViewModel.kt
+++ app/src/main/java/com/josycom/mayorjay/genpass/home/generate/GeneratePasswordViewModel.kt
@@ -1,18 +1,45 @@
 package com.josycom.mayorjay.genpass.home.generate
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.josycom.mayorjay.genpass.data.getPasswordCharacters
+import com.josycom.mayorjay.genpass.data.getPasswordDisplayTexts
+import com.josycom.mayorjay.genpass.util.Constants
+import org.apache.commons.lang3.StringUtils
+import java.security.SecureRandom
 
 class GeneratePasswordViewModel : ViewModel() {
 
-    private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
+     val passwordType = MutableLiveData<String>()
+     val passwordLength = MutableLiveData<String>()
+     val password = MutableLiveData<String>()
+
+    fun getPasswordTypes(): MutableList<String> = getPasswordDisplayTexts()
+
+    fun validateInputs(): String {
+        if (StringUtils.isBlank(passwordType.value) || StringUtils.equalsIgnoreCase(passwordType.value, Constants.SELECT_PASSWORD_TYPE)) {
+            return Constants.SELECT_PASSWORD_TYPE
+        }
+
+        if (passwordLength.value == null) {
+            return "Password Length cannot be blank"
+        }
+
+        if (passwordLength.value?.toInt() ?: 0 < 15 || passwordLength.value?.toInt() ?: 0 > 64) {
+            return "Please input a value between 15 and 64"
+        }
+        return StringUtils.EMPTY
     }
 
-    fun setIndex(index: Int) {
-        _index.value = index
+    fun generatePassword(passwordType: String, passwordLength: Int): String {
+        val buffer = StringBuffer()
+        val characters = getPasswordCharacters(passwordType)
+        val charactersLength = characters.length
+        val secureRandom = SecureRandom()
+        for (i in 0 until passwordLength) {
+            val index = secureRandom.nextInt(charactersLength).toDouble()
+            buffer.append(characters[index.toInt()])
+        }
+        return buffer.toString()
     }
 }
